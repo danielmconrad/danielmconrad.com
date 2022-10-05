0b909ea6c260fc06aa0f8928bc114c957e96442d
Daniel Conrad <daniel.m.conrad@gmail.com>
2022-10-04 21:47:35 -0500

    Moves some files around

---
+++ app/src/lib/StartHere.svelte
@@ -0,0 +1,4 @@
+<!-- https://svelte.dev/docs -->
+<!-- https://github.com/jorgegorka/svelte-router -->
+<!-- https://github.com/danielmconrad/www.danielmconrad.com/blob/master/src/components/Header/Header.css -->
+<!-- https://vitejs.dev/guide/#browser-support -->
